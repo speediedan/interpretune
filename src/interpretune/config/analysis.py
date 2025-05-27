8b0406aa0e548c9a81f948c7e38aac8e500497e9
Daniel Dale <danny.dale@gmail.com>
2025-05-27 12:34:47 -0700

    updated default IT_ANALYSIS_CACHE to XDG_CACHE_HOME/interpretune

--- src/interpretune/config/analysis.py
+++ src/interpretune/config/analysis.py
@@ -6,7 +6,7 @@ import os
 import datetime
 import warnings
 
-from datasets.config import HF_CACHE_HOME
+from datasets.config import XDG_CACHE_HOME
 from transformers import BatchEncoding, PreTrainedTokenizerBase
 
 from interpretune.analysis import (SAEAnalysisTargets, resolve_names_filter, _make_simple_cache_hook, OpSchema,
@@ -18,7 +18,7 @@ from interpretune.utils import DEFAULT_DECODE_KWARGS
 from interpretune.utils import rank_zero_warn, rank_zero_debug
 
 IT_ANALYSIS_CACHE_DIR = "interpretune"
-DEFAULT_IT_ANALYSIS_CACHE = os.path.join(HF_CACHE_HOME, IT_ANALYSIS_CACHE_DIR)
+DEFAULT_IT_ANALYSIS_CACHE = os.path.join(XDG_CACHE_HOME, IT_ANALYSIS_CACHE_DIR)
 IT_ANALYSIS_CACHE = Path(os.getenv("IT_ANALYSIS_CACHE_DIR", DEFAULT_IT_ANALYSIS_CACHE))
 
 @dataclass(kw_only=True)
